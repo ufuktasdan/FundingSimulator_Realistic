0ad070592a3fa761188c8498d1540348e07521dd
ufuktasdan <71287579+ufuktasdan@users.noreply.github.com>
2025-01-13 00:48:50 +0000

    Update More_realistic.py

--- More_realistic.py
+++ More_realistic.py
@@ -8,7 +8,8 @@ Original file is located at
 
 **Explanation: ** This is an extension of Scientific Community simulation forked from !git clone https://github.com/shaharavin/FundingSimulator.git
 
-The old code renewed into new python version 3.11 in google colab. Then the packages are reduced for supercomputer, the Hamilton, of Durham university.
+The old code was renewed into the new python version 3.11 in Google Colab. 
+Then the packages are reduced for the supercomputer, the Hamilton, of Durham university.
 """
 
 !nvidia-smi  # this should display information about available GPUs
